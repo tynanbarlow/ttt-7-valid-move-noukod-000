fa6e4ca222425f4b46da13460d64e8c3c1a2491d
Tynan Barlow <tynan@noukod.org>
2018-09-08 19:11:54 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,8 +8,8 @@ def position_taken?(board, index)
   end
 end
 
-def valid_move? (board, index)
-  if position_taken? (board, index)
+def valid_move?(board, index)
+  if position_taken?(board, index)
     return false
   else
     if index <= 8 && index >= 0
